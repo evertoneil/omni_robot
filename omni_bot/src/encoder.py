fcc244f3984bf8404273c0a73de7b5f20f8f4c76
evertoneil <63706337+evertoneil@users.noreply.github.com>
2020-05-05 10:33:42 +0700

    Add files via upload

--- omni_bot/src/encoder.py
+++ omni_bot/src/encoder.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Int16
 import RPi.GPIO as GPIO
+from omni_bot.msg import jarak
 
 # Set the GPIO modes
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Set variables for the GPIO motor pins
-motor1_encodera = 5
-motor1_encoderb = 6
-motor2_encodera = 22
-motor2_encoderb = 25
+#=========================================
+motor1_encodera = 22
+motor1_encoderb = 25
+motor2_encodera = 5
+motor2_encoderb = 6
 motor3_encodera = 17
 motor3_encoderb = 27
 
@@ -22,7 +23,7 @@ GPIO.setup(motor2_encodera,GPIO.IN)
 GPIO.setup(motor2_encoderb,GPIO.IN)
 GPIO.setup(motor3_encodera,GPIO.IN)
 GPIO.setup(motor3_encoderb,GPIO.IN)
-
+#=====================================================
 outcome = [0,-1,1,0,-1,0,0,1,1,0,0,-1,0,-1,1,0]
 last_AB = 0b00
 last_CD = 0b00
@@ -30,43 +31,84 @@ last_EF = 0b00
 counter1 = 0
 counter2 = 0
 counter3 = 0
+rotation3 = 0
+rotation2 = 0
+rotation1 = 0
 
-def Encode1():
+#=================================================
+def Encode1(): #fix
     global last_AB
     global counter1
+    global rotation1
     A = GPIO.input(motor1_encodera)
     B = GPIO.input(motor1_encoderb)
     current_AB = (A << 1) | B
     position = (last_AB << 2) | current_AB
     counter1 += outcome[position]
     last_AB = current_AB
-    print("A="+ str(counter1))
-
+    rotation1 = counter1/60
+#=================================================
 def Encode2():
     global last_CD
     global counter2
+    global rotation2
     C = GPIO.input(motor2_encodera)
     D = GPIO.input(motor2_encoderb)
     current_CD = (C << 1) | D
     position = (last_CD << 2) | current_CD
     counter2 += outcome[position]
     last_CD = current_CD
-    print("B="+ str(counter2))
-
+    rotation2 = counter2/60
+#==================================================
 def Encode3():
     global last_EF
     global counter3
+    global rotation3
     E = GPIO.input(motor3_encodera)
     F = GPIO.input(motor3_encoderb)
     current_EF = (E << 1) | F
     position = (last_EF << 2) | current_EF
     counter3 += outcome[position]
     last_EF = current_EF
-    print("C="+ str(counter3))
+    rotation3 = counter3/60
+#==============================================
+def talker() :
+    pub = rospy.Publisher('odometry',jarak,queue_size=10)
+    rospy.init_node('encode', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    value = jarak()
+    while not rospy.is_shutdown():
+    	Encode3()
+    	#Encode1()
+   	Encode2()
+	value.enc3 = rotation3
+	#value.enc1 = rotation1
+	value.enc2 = rotation2
+	pub.publish(value)
+"""
+def talker():
+    pub = rospy.Publisher('odometry',jarak,queue_size=10)
+    rospy.init_node('encode', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+	#Encode1()
+	#Encode2()
+	#Encode3()
+	value = jarak()
+    	value.enc1 = counter1
+    	#value.enc2 = rotation2
+    	#value.enc3 = rotation3
+ 	print(value.enc1)
+	#rospy.loginfo(value)
+        pub.publish(value)
+        rate.sleep()
 
-while True:
-    Encode2()
-    Encode3()
-    Encode1()
+"""
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
+
 
 
--- omni_bot/src/inverse.py
+++ omni_bot/src/inverse.py
@@ -3,20 +3,28 @@
 import rospy
 from omni_bot.msg import nilai
 import math as m
+import time
+
+time_start = time.time()
+second = 5
 
 R = 0.12 # Length from Wheel to Center Point (m)
 r = 0.04 # Wheel's Radius (m)
 
-Theta_d = 30 #in Degree
+Theta_d = 0 #in Degree
 Theta = m.radians(Theta_d)
 
-x=1
-y=2
-degree=30
+x = input("X value: ")
+y = input("Y value: ")
+degree = input("Rotate: ")
 
 a1_d = 0 #degree
 a2_d = 120 #degree
 a3_d = 240
+
+x = int(x)
+y = int(y)
+degree = int(degree)
 
 a1 = m.radians(a1_d)
 a2 = m.radians(a2_d)
@@ -26,19 +34,28 @@ v1 = ((-m.sin(Theta)*m.cos(Theta)*x) + (m.sqrt(m.cos(Theta))*y) + R*degree)/ r
 v2 = ((-m.sin(Theta+a2)*m.cos(Theta)*x) + (m.cos(Theta+a2)*m.cos(Theta)*y)+ R*degree)/ r
 v3 = ((-m.sin(Theta+a3)*m.cos(Theta)*x) + (m.cos(Theta+a3)*m.cos(Theta)*y)+ R*degree)/ r
 
+vmotor1 = (v1/10)*33
+vmotor2 = (v2/10)*30
+vmotor3 = (v3/10)*60
+
 def talker():
     pub = rospy.Publisher('Value',nilai,queue_size=10)
     rospy.init_node('inverse', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     value = nilai()
-    value.v1 = v1
-    value.v2 = v2
-    value.v3 = v3
-    
+    value.v1 = vmotor1
+    value.v2 = vmotor2
+    value.v3 = vmotor3
     while not rospy.is_shutdown():
  	rospy.loginfo(value)
         pub.publish(value)
         rate.sleep()
+
+def test():
+    global time_start
+    global second
+    if (time.time()-time_start) > second:
+	exit()
 
 if __name__ == '__main__':
     try:
