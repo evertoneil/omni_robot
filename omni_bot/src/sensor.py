42034f2c320671196f331513f6f6dd195ffe6081
evertoneil <63706337+evertoneil@users.noreply.github.com>
2020-05-29 18:37:11 +0700

    Add Some Comments

--- omni_bot/src/sensor.py
+++ omni_bot/src/sensor.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
-import spidev # To communicate with SPI devices
-from numpy import interp    # To scale values
-from time import sleep  # To add delay
-import RPi.GPIO as GPIO # To use GPIO pins
+
+import spidev 			# To communicate with SPI devices
+from numpy import interp    	# To scale values
+from time import sleep  	# To add delay
+import RPi.GPIO as GPIO
 import rospy
-from std_msgs.msg import Float32
+from omni_bot.msg import jarak
 
 spi = spidev.SpiDev() # Created an object
 spi.open(0,0)
-   
+
+#================Read Analog Value===================
 def analogInput(channel):
   spi.max_speed_hz = 1350000
   adc = spi.xfer2([1,(8+channel)<<4,0])
   data = ((adc[1]&3) << 8) + adc[2]
   return data
-
+#=====================================================
 def talker():
-    pub = rospy.Publisher('distance', Float32, queue_size=10)
+    pub = rospy.Publisher('distance', jarak, queue_size=10) 
     rospy.init_node('sensor', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+    kirim = jarak()
     while not rospy.is_shutdown():
- 	output = analogInput(0) # Reading from CH0
-	cm = pow(4027.4/output,1.2134)
-    	#cm = pow(3027.4/output,1.2)
-	if cm > 80:
+ 	output = analogInput(0) 	# Reading from CH0
+	cm = 12320*pow(output,-1.073)
+        if cm > 80:			#set limit to 80 cm
 		cm = 80
-        rospy.loginfo(cm)
-	pub.publish(cm)
+        kirim.cm = cm
+	pub.publish(kirim)		#send data to master
         rate.sleep()
 
 if __name__ == '__main__':
