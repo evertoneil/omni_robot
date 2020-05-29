89cfeb177983f64c6ae1c9535870974514e8df4c
evertoneil <63706337+evertoneil@users.noreply.github.com>
2020-05-29 18:40:30 +0700

    Simplify Program & Add Comments

--- omni_bot/src/encoder.py
+++ omni_bot/src/encoder.py
@@ -3,44 +3,40 @@
 import rospy
 import RPi.GPIO as GPIO
 from omni_bot.msg import counter
+import time
 
 # Set the GPIO modes
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Set variables for the GPIO motor pins
-#=========================================
+#==================================================
 motor1_encodera = 5
 motor1_encoderb = 6 
 motor2_encodera = 27 
 motor2_encoderb = 17 
 motor3_encodera = 25
 motor3_encoderb = 22
-
+#--------------------------------------------
 GPIO.setup(motor1_encodera,GPIO.IN)
 GPIO.setup(motor1_encoderb,GPIO.IN)
 GPIO.setup(motor2_encodera,GPIO.IN)
 GPIO.setup(motor2_encoderb,GPIO.IN)
 GPIO.setup(motor3_encodera,GPIO.IN)
 GPIO.setup(motor3_encoderb,GPIO.IN)
-#=====================================================
-outcome = [0,-1,1,0,-1,0,0,1,1,0,0,-1,0,-1,1,0]
+#============VARIABLE==============================
+outcome = [0,1,-1,0,-1,0,0,1,1,0,0,-1,0,-1,1,0]
 last_AB = 0b00
 last_CD = 0b00
 last_EF = 0b00
 counter1 = 0
 counter2 = 0
 counter3 = 0
-rotation3 = 0
-rotation2 = 0
-rotation1 = 0
 count = counter()
-
-#=================================================
-def Encode1(): #fix
-    global last_AB
-    global counter1
-    global rotation1
+#==================================================
+#=================COUNTER==========================
+def Encode1():					#Counter Motor 1
+    global last_AB,counter1				
     A = GPIO.input(motor1_encodera)
     B = GPIO.input(motor1_encoderb)
     current_AB = (A << 1) | B
@@ -49,10 +45,8 @@ def Encode1(): #fix
     last_AB = current_AB
     count.enc1 = counter1
 #=================================================
-def Encode2():
-    global last_CD
-    global counter2
-    global rotation2
+def Encode2():					#Counter Motor 2
+    global last_CD,counter2
     C = GPIO.input(motor2_encodera)
     D = GPIO.input(motor2_encoderb)
     current_CD = (C << 1) | D
@@ -60,11 +54,9 @@ def Encode2():
     counter2 += outcome[position]
     last_CD = current_CD
     count.enc2 = counter2
-#==================================================
-def Encode3():
-    global last_EF
-    global counter3
-    global rotation3
+#=================================================
+def Encode3():					#Counter Motor 3
+    global last_EF,counter3
     E = GPIO.input(motor3_encodera)
     F = GPIO.input(motor3_encoderb)
     current_EF = (E << 1) | F
@@ -72,16 +64,16 @@ def Encode3():
     counter3 += outcome[position]
     last_EF = current_EF
     count.enc3 = counter3
-#==============================================
+#====================================================
+#=====================================================
 def talker() :
-    pub = rospy.Publisher('counter',counter,queue_size=10)
+    pub = rospy.Publisher('counter',counter,queue_size=5)
     rospy.init_node('encode', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-    	Encode3()
-    	Encode1()
-   	Encode2()
-	pub.publish(count)
+	Encode1()
+	Encode3()
+	Encode2()
+	pub.publish(count)			#Send Motors pulses
 #============================================
 if __name__ == '__main__':
     try:
